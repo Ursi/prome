34f81b8818a7122e152d5d44db8a098b875048cb
Mason Mackaman <masondeanm@aol.com>
2019-06-30 18:18:41 -0400

    testing

--- prome.js
+++ prome.js
@@ -1,4 +1,4 @@
-export default {
+/*export default {
     bookmarks: {
         getChildren(id) {
             return new Promise(resolve => {
@@ -47,4 +47,59 @@ export default {
             });
         },
     },
-};
+};*/
+
+function getPath(obj, ...path) {
+    let value = obj;
+    for (let pathPart of path) {
+        value = value[pathPart];
+    }
+
+    return value;
+},
+
+function makePromise(path, cbPos) {
+    return function(...args){
+        let method = chrome;
+        let methodThis;
+        let method = getPath(chrome, path);
+        let methodThis = getPath(chrome, path.slice(0, -1));
+        return new Promise(resolve => {
+            args[cbPos] = resolve;
+            mthod.call(methodThis, ...args);
+        });
+    }
+}
+
+let promiseMe =  {
+    bookmarks: {
+        getChildren: 1,
+        move: 2,
+    },
+    storage: {
+        sync: {
+            get: 1,
+            set: 1,
+        },
+    },
+    tabs: {
+        get: 1,
+        query: 1,
+        sendMessage: 3,
+        update: 2,
+    },
+}
+
+let prome = {};
+(function makeProme(obj, path = []) {
+    for (let [p, v] of Object.entries(obj)) {
+        if (typeof v == 'number') {
+            getPath(prome, path.slice(0, -1))[p] = makePromeise(path, v);
+        } else {
+            prome[p] = {};
+            makeProme(v, path.concat(p));
+        }
+    }
+})(promiseMe);
+
+export default prome;
