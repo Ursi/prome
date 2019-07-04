ccf64ce1d89848bb32bdefe70e8790ee269f30d3
Mason Mackaman <masondeanm@aol.com>
2019-07-04 15:58:24 -0400

    Improve code
    Made the code much more efficient.

--- prome.js
+++ prome.js
@@ -1,77 +1,24 @@
-/*export default {
-    bookmarks: {
-        getChildren(id) {
-            return new Promise(resolve => {
-                chrome.bookmarks.getChildren(id, resolve);
-            });
-        },
-        move(id, destination) {
-            return new Promise(resolve => {
-                chrome.bookmarks.move(id, destination, resolve);
-            });
-        },
-    },
-    storage: {
-        sync: {
-            get(keys) {
-                return new Promise(resolve => {
-                    chrome.storage.sync.get(keys, resolve);
-                });
-            },
-            set(items) {
-                return new Promise(resolve => {
-                    chrome.storage.sync.set(items, resolve);
-                });
-            },
-        },
-    },
-    tabs: {
-        get(tabId) {
-            return new Promise(resolve => {
-                chrome.tabs.get(tabId, resolve);
-            });
-        },
-        query(queryInfo) { //chrome.tabs.query(object queryInfo, function callback)
-            return new Promise(resolve => {
-                chrome.tabs.query(queryInfo, resolve);
-            });
-        },
-        sendMessage(tabId, message, options) {
-            return new Promise(resolve => {
-                chrome.tabs.sendMessage(tabId, message, options, resolve);
-            });
-        },
-        update(tabId, updateProperties) {
-            return new Promise(resolve => {
-                chrome.tabs.update(tabId, updateProperties, resolve);
-            });
-        },
-    },
-};*/
-
-function getPath(obj, ...path) {
+function getPath(obj, path) {
     let value = obj;
     for (let pathPart of path) {
         value = value[pathPart];
     }
 
     return value;
-},
+}
 
 function makePromise(path, cbPos) {
-    return function(...args){
-        let method = chrome;
-        let methodThis;
+    return function(...args) {
         let method = getPath(chrome, path);
         let methodThis = getPath(chrome, path.slice(0, -1));
         return new Promise(resolve => {
             args[cbPos] = resolve;
-            mthod.call(methodThis, ...args);
+            method.call(methodThis, ...args);
         });
     }
 }
 
-let promiseMe =  {
+let promiseMe =  {//the number is the index of the callback
     bookmarks: {
         getChildren: 1,
         move: 2,
@@ -94,9 +41,9 @@ let prome = {};
 (function makeProme(obj, path = []) {
     for (let [p, v] of Object.entries(obj)) {
         if (typeof v == 'number') {
-            getPath(prome, path.slice(0, -1))[p] = makePromeise(path, v);
+            getPath(prome, path)[p] = makePromise(path.concat([p]), v);
         } else {
-            prome[p] = {};
+            getPath(prome, path)[p] = {};
             makeProme(v, path.concat(p));
         }
     }
