26ac6303970075b020e3abbcb81bf99a0799081e
Mason Mackaman <masondeanm@aol.com>
2019-07-16 12:42:52 -0400

    Change all whitespace to tabs
    Unbeknownst to me, I was mixing up tabs and spaces because Atom made the
    spaces seem like tabs.

--- prome.js
+++ prome.js
@@ -1,52 +1,52 @@
 function getPath(obj, path) {
-    let value = obj;
-    for (let pathPart of path) {
-        value = value[pathPart];
-    }
+	let value = obj;
+	for (let pathPart of path) {
+		value = value[pathPart];
+	}
 
-    return value;
+	return value;
 }
 
 function makePromise(path, cbPos) {
-    return function(...args) {
-        let methodThis = getPath(chrome, path.slice(0, -1));
-        let method = methodThis[path[path.length - 1]];
-        return new Promise(resolve => {
-            args[cbPos] = resolve;
-            method.apply(methodThis, args);
-        });
-    }
+	return function(...args) {
+		let methodThis = getPath(chrome, path.slice(0, -1));
+		let method = methodThis[path[path.length - 1]];
+		return new Promise(resolve => {
+			args[cbPos] = resolve;
+			method.apply(methodThis, args);
+		});
+	}
 }
 
 let promiseMe =  {//the number is the index of the callback
-    bookmarks: {
-        getChildren: 1,
-        move: 2,
-    },
-    storage: {
-        sync: {
-            get: 1,
-            set: 1,
-        },
-    },
-    tabs: {
-        get: 1,
-        query: 1,
-        sendMessage: 3,
-        update: 2,
-    },
+	bookmarks: {
+		getChildren: 1,
+		move: 2,
+	},
+	storage: {
+		sync: {
+			get: 1,
+			set: 1,
+		},
+	},
+	tabs: {
+		get: 1,
+		query: 1,
+		sendMessage: 3,
+		update: 2,
+	},
 }
 
 let prome = {};
 (function makeProme(obj, path = []) {
-    for (let [p, v] of Object.entries(obj)) {
-        if (typeof v == 'number') {
-            getPath(prome, path)[p] = makePromise(path.concat([p]), v);
-        } else {
-            getPath(prome, path)[p] = {};
-            makeProme(v, path.concat(p));
-        }
-    }
+	for (let [p, v] of Object.entries(obj)) {
+		if (typeof v == 'number') {
+			getPath(prome, path)[p] = makePromise(path.concat([p]), v);
+		} else {
+			getPath(prome, path)[p] = {};
+			makeProme(v, path.concat(p));
+		}
+	}
 })(promiseMe);
 
 export default prome;
