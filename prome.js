c67c5b5d8ca5cd561de9b4e3f2381b80968370c4
Mason Mackaman <masondeanm@aol.com>
2019-09-18 18:57:59 -0400

    Change prome to be used as a background script

--- prome.js
+++ prome.js
@@ -1,55 +1,57 @@
-function getPath(obj, path) {
-	let value = obj;
-	for (let pathPart of path) {
-		value = value[pathPart];
-	}
-
-	return value;
-}
-
-function makePromise(path, cbPos) {
-	return function(...args) {
-		let methodThis = getPath(chrome, path.slice(0, -1));
-		let method = methodThis[path[path.length - 1]];
-		return new Promise(resolve => {
-			args[cbPos] = resolve;
-			method.apply(methodThis, args);
-		});
-	}
-}
-
-let promiseMe =  {//the number is the index of the callback
-	bookmarks: {
-		getChildren: 1,
-		move: 2,
-	},
-	browsingData: {
-		remove: 2
-	},
-	storage: {
-		sync: {
-			get: 1,
-			set: 1,
+const prome = {};
+(()=>{
+	const promiseMe =  {//the number is the index of the callback
+		bookmarks: {
+			getChildren: 1,
+			move: 2,
 		},
-	},
-	tabs: {
-		get: 1,
-		query: 1,
-		sendMessage: 3,
-		update: 2,
-	},
-}
+		browsingData: {
+			remove: 2
+		},
+		storage: {
+			sync: {
+				get: 1,
+				set: 1,
+			},
+		},
+		tabs: {
+			get: 1,
+			query: 1,
+			sendMessage: 3,
+			update: 2,
+		},
+	}
 
-let prome = {};
-(function makeProme(obj, path = []) {
-	for (let [p, v] of Object.entries(obj)) {
-		if (typeof v == 'number') {
-			getPath(prome, path)[p] = makePromise(path.concat([p]), v);
-		} else {
-			getPath(prome, path)[p] = {};
-			makeProme(v, path.concat(p));
+	function getPath(obj, path) {
+		let value = obj;
+		for (let pathPart of path) {
+			value = value[pathPart];
+		}
+
+		return value;
+	}
+
+	function makePromise(path, cbPos) {
+		return function(...args) {
+			const
+				methodThis = getPath(chrome, path.slice(0, -1)),
+				method = methodThis[path[path.length - 1]];
+
+			return new Promise(resolve => {
+				args[cbPos] = resolve;
+				method.apply(methodThis, args);
+			});
 		}
 	}
-})(promiseMe);
 
-export default prome;
+	(function makeProme(obj, path = []) {
+		for (let [p, v] of Object.entries(obj)) {
+			if (typeof v === `number`) {
+				getPath(prome, path)[p] = makePromise(path.concat([p]), v);
+			} else {
+				getPath(prome, path)[p] = {};
+				makeProme(v, path.concat(p));
+			}
+		}
+	})(promiseMe);
+})();
