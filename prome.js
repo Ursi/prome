efd6086ca71c3e23ca6e1c9c43269c25b3072658
Mason Mackaman <masondeanm@aol.com>
2019-09-24 22:59:18 -0400

    Fix bug when nesting

--- prome.js
+++ prome.js
@@ -1,32 +1,37 @@
-const prome = new Proxy(()=>{}, {
-	apply(targ, thisArg, args) {
-		if (thisArg === targ) {
-			if (!targ.chromeMethod) targ.chromeMethod = chrome;
-			targ.chromeThis = targ.chromeMethod;
-			targ.chromeMethod = targ.chromeMethod[args[0]];
-		} else {
-			const {
-				chromeMethod,
-				chromeThis,
-			} = targ;
+const prome = new Proxy({}, {
+	// the double proxy is required for nesting
+	get(_, prop) {
+		return new Proxy(()=>{}, {
+			apply(targ, thisArg, args) {
+				if (thisArg === targ) {
+					if (!targ.chromeMethod) targ.chromeMethod = chrome;
+					targ.chromeThis = targ.chromeMethod;
+					targ.chromeMethod = targ.chromeMethod[args[0]];
+				} else {
+					const {
+						chromeMethod,
+						chromeThis,
+					} = targ;
 
-			delete targ.chromeMethod;
-			delete targ.chromeThis;
-			return new Promise((resolve, reject) => {
-				try {
-					chromeMethod.call(chromeThis, ...args, resolve);
-				} catch (error) {
-					reject(error);
+					delete targ.chromeMethod;
+					delete targ.chromeThis;
+					return new Promise((resolve, reject) => {
+						try {
+							chromeMethod.call(chromeThis, ...args, resolve);
+						} catch (error) {
+							reject(error);
+						}
+					});
 				}
-			});
-		}
-	},
-	get(targ, prop) {
-		if (prop === Symbol.toPrimitive) {// chromium dev tools likes to call this
-			return ()=> `prome`;
-		} else {
-			Function.prototype.call.call(prome, targ, prop);
-			return prome;
-		}
-	},
+			},
+			get(targ, prop, rec) {
+				if (prop === Symbol.toPrimitive) {// chromium dev tools likes to call this
+					return ()=> `prome`;
+				} else {
+					Function.prototype.call.call(rec, targ, prop);
+					return rec;
+				}
+			},
+		})[prop];
+	}
 });
